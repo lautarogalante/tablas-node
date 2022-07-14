93a5e73379390399b69985430934100084f21b80
Lautaro <lautaro9galante@gmail.com>
2022-07-14 18:43:15 -0300

    Aprendiendo nodejs

---
+++ app.js
@@ -0,0 +1,19 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+
+const { alias } = require('yargs');
+const { crearArchivo, listar } = require('./multiplicar/multiplicar');
+let comando = argv._[0];
+
+switch(comando){
+    case 'listar':
+        listar(argv.base, argv.limite)
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log('Archivo creado: ', colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+    default:
+        console.log('Comando no reconocido')
+}
